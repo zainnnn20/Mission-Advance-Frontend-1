689eb4f7ef2972c2c61892723159036ce52fa871
zainnnn20 <aisyzain26@gmail.com>
2025-08-15 21:13:59 +0700

    Fi Mission Intermediate Front End 1A

--- src/Beranda.jsx
+++ src/Beranda.jsx
@@ -23,7 +23,7 @@ import avatarJenna8 from './assets/image/8.png';
 import avatarJenna9 from './assets/image/9.png';
 import ratingIcon from './assets/image/Rating.png';
 import userProfileImageDesktop from './assets/image/user.png'; 
-import userProfileImageMobile from './assets/image/vector.png';
+import userProfileImageMobile from './assets/image/Vector.png';
 import arrowIcon from './assets/image/Vector 3.png';
 
 const SearchIcon = () => ( <svg className="icon" xmlns="http://www.w.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg> );
